0fd36a2988da4bd7ae83e8a826e451122723044e
ICTS - JANIER <janierfsilva@gmail.com.br>
2023-06-28 14:19:05 -0400

    exercício de variáveis

---
+++ src/academy_devdojo/Aula03VariaveisPrimitivas.java
@@ -0,0 +1,14 @@
+package academy_devdojo;
+
+//meu jeito de fazer antes do professor
+public class Aula03VariaveisPrimitivas {
+    public static void main(String[] args) {
+        String nome = "Janier";
+        String endereco = "Rua Crispim do Amaral";
+        double  salario = 9000.55;
+        String data = "26/06/2023";
+        //String resumao = ""; a única linha que ficou diferente da do professor (Ihuuu!!)
+        System.out.println("Eu, "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de "+salario+", na data "+data);
+    }
+
+}
